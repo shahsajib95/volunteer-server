28ada395fad0046dba8fe6f85e90a4c8033fba13
Shahariar Rahman Sajib <61103469+shahsajib95@users.noreply.github.com>
2020-10-29 09:59:15 +0600

    add

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ admin.initializeApp({
 });
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lirp7.mongodb.net/${env.process.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lirp7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
